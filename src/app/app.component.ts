c6e1df8469670fbd4bcda97069654669a419c230
ivanletralok <ivanletralok@gmail.com>
2023-03-07 06:32:29 -0500

    feat: init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MessageService } from "primeng/api";
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task';
+  product: any;
+  submitted: boolean = false;
+  productDialog: boolean = false;
+
+  tasks: any[] = [];
+  selectedProducts: any[] = [];
+  taskForm!: FormGroup;
+  task: any ;
+
+
+
+  constructor(
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService, private fb: FormBuilder) {
+    this.initForm();
+  }
+  ngOnInit(): void {
+
+    // this.tasks.push(this.task);
+  }
+
+
+  initForm(): void {
+    this.taskForm = this.fb.group({
+      id: [null],
+      description: [],
+      perfil: [],
+      status: []
+    });
+  }
+
+  openNew() {
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  deleteSelectedProducts() {
+  }
+  onSave(): void {
+  const form = Object.assign({}, this.taskForm.value);
+  form.id = Math.random();
+  this.task = form;
+    console.log('form', this.taskForm.value)
+  console.log('task', this.task);
+    this.tasks.push({...this.task, form});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DialogModule,
+    ToastModule,
+    ToolbarModule,
+    TableModule,
+    ConfirmDialogModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [MessageService, ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
